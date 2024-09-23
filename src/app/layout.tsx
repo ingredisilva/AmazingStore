bd78a6e98e4ea01f3bbd92a0b5a6d75aaf5f4500
Ingredi Silva <55279716+ingredisilva@users.noreply.github.com>
2024-09-23 18:41:28 -0300

    adds animations to the pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import GlobalStyle from "./styles/globalsStyles";
 import { Provider } from "react-redux";
 import store from "@/lib/store";
 import Header from "./components/Header";
+import { AnimatePresence, motion } from "framer-motion";
+import router from "next/router";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,9 +30,20 @@ export default function RootLayout({
     <Provider store={store}>
       <html lang="en">
         <body className={`${geistSans.variable} ${geistMono.variable}`}>
+
           <Header />
           <GlobalStyle />
-          {children}
+          <AnimatePresence exitBeforeEnter>
+            <motion.div
+              key={router.route}
+              initial={{ opacity: 0, x: 100 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -100 }}
+              transition={{ duration: 0.5 }}
+            >
+              {children}
+            </motion.div>
+          </AnimatePresence>
         </body>
       </html>
     </Provider>
